9d289f0d8726a8153ca2935a1392b6e3b5ae2cad
Jon Chen <bsd@voltaire.sh>
2018-06-22 00:36:46 -0700

    implements lambda custom handler

---
+++ lambdas/handler.go
@@ -0,0 +1,27 @@
+package lambdas
+
+import (
+	"context"
+
+	"github.com/bsdlp/copbot/commands"
+	"github.com/golang/protobuf/proto"
+)
+
+// HandlerFunc implements lambda.Handler
+type HandlerFunc func(context.Context, commands.InboundMessage) (commands.OutboundMessage, error)
+
+// Invoke implements lambda.Handler and handles protobuf marshal/unmarshal
+func (hf HandlerFunc) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
+	inbound := &commands.InboundMessage{}
+	err := proto.Unmarshal(payload, inbound)
+	if err != nil {
+		return nil, err
+	}
+
+	outbound, err := hf(ctx, inbound)
+	if err != nil {
+		return nil, err
+	}
+
+	return proto.Marshal(outbound)
+}
