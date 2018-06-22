38d9c73ddb5a0f3f327b76533e03a2899d95ffd3
Jon Chen <bsd@voltaire.sh>
2018-06-22 01:16:37 -0700

    rename HandlerFunc to MessageHandlerFunc

--- lambdas/message_handler.go
+++ lambdas/message_handler.go
@@ -7,11 +7,11 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
-// HandlerFunc implements lambda.Handler
-type HandlerFunc func(context.Context, *commands.InboundMessage) (*commands.OutboundMessage, error)
+// MessageHandlerFunc implements lambda.Handler
+type MessageHandlerFunc func(context.Context, *commands.InboundMessage) (*commands.OutboundMessage, error)
 
 // Invoke implements lambda.Handler and handles protobuf marshal/unmarshal
-func (hf HandlerFunc) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
+func (hf MessageHandlerFunc) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
 	inbound := &commands.InboundMessage{}
 	err := proto.Unmarshal(payload, inbound)
 	if err != nil {
